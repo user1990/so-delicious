9d8a388573a805817c47b6314a5369c5139b2dd2
user1990 <paulius.rimg1990@gmail.com>
2017-06-21 14:03:03 +0300

    # Creating User Accounts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -26,11 +28,14 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
-// router.get('/login', userController.loginForm);
-// router.get('/register', userController.registerForm);
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
 // 2. register the user
 // 3. we need to log them in
+router.post('/register',
+  userController.validateRegister
+);
 
 module.exports = router;
