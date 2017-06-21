90d90a2d11321236902ee6421457411812520486
user1990 <paulius.rimg1990@gmail.com>
2017-06-21 16:59:37 +0300

    # Creating a User Account Edit Screen

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,15 +19,15 @@ exports.validateRegister = (req, res, next) => {
     remove_extension: false,
     gmail_remove_subaddress: false
   });
-  req.checkBody('password', 'Pasword Cannot be Blank!').notEmpty();
-  req.checkBody('password-confirm', 'Confirmed Password cannot be Blank!').notEmpty();
+  req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
+  req.checkBody('password-confirm', 'Confirmed Password cannot be blank!').notEmpty();
   req.checkBody('password-confirm', 'Oops! Your passwords do not match').equals(req.body.password);
 
   const errors = req.validationErrors();
   if (errors) {
     req.flash('error', errors.map(err => err.msg));
     res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
-    return; // stop the fn from runing
+    return; // stop the fn from running
   }
   next(); // there were no errors!
 };
@@ -37,4 +37,23 @@ exports.register = async (req, res, next) => {
   const register = promisify(User.register, User);
   await register(user, req.body.password);
   next(); // pass to authController.login
-}
+};
+
+exports.account = (req, res) => {
+  res.render('account', { title: 'Edit Your Account' });
+};
+
+exports.updateAccount = async (req, res) => {
+  const updates = {
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  const user = await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: 'query' }
+  );
+  req.flash('success', 'Updated the profile!');
+  res.redirect('back');
+};
