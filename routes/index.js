e6158ab19d12ed261c09d1838a1e81757f097407
user1990 <paulius.rimg1990@gmail.com>
2017-06-22 16:16:00 +0300

    # Hearts: Pushing User Data to our API

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+// const reviewController = require('../controllers/reviewController');
 
 const { catchErrors } = require('../handlers/errorHandlers');
 
@@ -53,6 +54,13 @@ router.post('/account/reset/:token',
 );
 
 router.get('/map', storeController.mapPage);
+// router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
+/* router.post('/reviews/:id',
+  authController.isLoggedIn,
+  catchErrors(reviewController.addReview)
+);
+
+router.get('/top', catchErrors(storeController.getTopStores));*/
 
 /*
   API
@@ -60,5 +68,6 @@ router.get('/map', storeController.mapPage);
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 module.exports = router;
